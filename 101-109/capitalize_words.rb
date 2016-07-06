a9b7fa15a149a29cecf1e7e22927653365150f2e
Rubén Lozano <contact@rubenlozano.com>
2016-07-06 12:48:40 -0500

    Add "Capitalize words" exercise

---
+++ 101-109/capitalize_words.rb
@@ -0,0 +1,20 @@
+=begin
+
+Write a method that takes a single String argument and returns a new string that
+contains the original value of the argument, but the first letter of every word
+is now capitalized.
+
+You may assume that words are any sequence of non-blank characters, and that
+only the first character of each word must be considered.
+
+=end
+
+def word_cap(string)
+  # string_capitalized = string.split.each { |word| word.capitalize! }
+  # string_capitalized.join(" ").scrub
+  string.split.map(&:capitalize).join(" ")
+end
+
+puts word_cap('four score and seven')    # => 'Four Score And Seven'
+puts word_cap('the javaScript language') # => 'The Javascript Language'
+puts word_cap('this is a "quoted" word') # => 'This Is A "quoted" Word'
