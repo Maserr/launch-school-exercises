34ee586e1ca95779bcb5030adbf56a5fd1a0655f
Rubén Lozano <contact@rubenlozano.com>
2016-08-10 09:45:26 -0500

    Refactor "List of digits" exercise
    Changed method in a more Ruby syntactic sugar way

--- 101-109/list_of_digits.rb
+++ 101-109/list_of_digits.rb
@@ -3,7 +3,8 @@
 # All of the tests below should print true.
 
 def digit_list(number)
-  number.to_s.chars.map { |num| num.to_i }
+  # number.to_s.chars.map { |num| num.to_i }
+  number.to_s.chars.map(&:to_i)
 end
 
 puts digit_list(12345) == [1, 2, 3, 4, 5]
