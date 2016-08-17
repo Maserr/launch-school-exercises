5f8858fd69b4c11b4d965a2e32f2f55028fc56cb
Rubén Lozano <contact@rubenlozano.com>
2016-08-16 21:51:16 -0500

    Add "Is prime?" exercise

---
+++ misc/is_prime.rb
@@ -0,0 +1,12 @@
+# Write a method that will determine whether an integer is a primer. Don't use
+# the Primer class.
+
+def is_prime?(integer)
+  (2..(integer - 1).each { |number| return false if integer % number == 0 }
+
+  true
+end
+
+puts is_prime?(2)  # => true
+puts is_prime?(25) # => false
+puts is_prime?(97) # => true
