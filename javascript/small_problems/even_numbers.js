ced434da187e0d49082b86ca6f65f3cbc3ce35df
Rubén Lozano <contact@rubenlozano.com>
2018-04-17 14:10:53 -0500

    Refactor even_numbers.js

--- javascript/small_problems/even_numbers.js
+++ javascript/small_problems/even_numbers.js
@@ -1,10 +1,6 @@
 // Log the even numbers from 1 to 99, inclusive, to the console. Log all numbers
 // on separate lines.
 
-for (var i = 1; i <= 99; i++) {
-  if (i % 2 !== 0) {
-    continue;
-  }
-
+for (var i = 2; i <= 99; i += 2) {
   console.log(i);
 }
